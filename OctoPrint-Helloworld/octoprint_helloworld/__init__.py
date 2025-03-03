c76be378d17e9a3e955bc0866438323fa0b8f541
Shafiq Rahman <shafiq@temple.edu>
2025-03-03 01:28:15 -0500

    Creating successfull plugins
    I have been getting good success with plugins

---
+++ OctoPrint-Helloworld/octoprint_helloworld/__init__.py
@@ -0,0 +1,10 @@
+import octoprint.plugin
+
+class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
+                       octoprint.plugin.TemplatePlugin):
+    def on_after_startup(self):
+        self._logger.info("Hello World!")
+
+__plugin_name__ = "Hello World"
+__plugin_pythoncompat__ = ">=3.7,<4"
+__plugin_implementation__ = HelloWorldPlugin()
